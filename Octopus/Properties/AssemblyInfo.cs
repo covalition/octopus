e9d5f6435b93ce47f6147864872ab67856554b9e
SND\kowaliszyn_cp <SND\kowaliszyn_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2009-02-20 09:24:59 +0000

    Minor changes
    git-svn-id: https://octopi.svn.codeplex.com/svn@12250 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- Octopus/Properties/AssemblyInfo.cs
+++ Octopus/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.1")]
-[assembly: AssemblyFileVersion("1.3.0.1")]
+[assembly: AssemblyVersion("1.3.0.2")]
+[assembly: AssemblyFileVersion("1.3.0.2")]
 [assembly: NeutralResourcesLanguageAttribute("")]
