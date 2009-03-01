896cb5678515e592179d206e7cc81d924a78924d
SND\kowaliszyn_cp <SND\kowaliszyn_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2009-03-01 10:26:51 +0000

    1.3 Beta
    git-svn-id: https://octopi.svn.codeplex.com/svn@13258 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- Octopus/Program.cs
+++ Octopus/Program.cs
@@ -11,7 +11,7 @@ namespace Octopus.CDIndex
 		static void Main()
 		{
             // Localization test
-            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("pt-br");
+            // System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("de");
 			Application.EnableVisualStyles();
             FrmMain f;
             using (FrmSplash splash = new FrmSplash()) {
