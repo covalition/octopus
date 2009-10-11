7d2cf704ea2134a94e1bbbfcf4043fceddf52b3b
SND\kowaliszyn_cp <SND\kowaliszyn_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2009-10-11 10:22:43 +0000

    git-svn-id: https://octopi.svn.codeplex.com/svn@29889 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- BlueMirrorIndexer/Properties/AssemblyInfo.cs
+++ BlueMirrorIndexer/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Blue Mirror")]
 [assembly: AssemblyProduct("Blue Mirror CD/DVD Indexer")]
-[assembly: AssemblyCopyright("Copyright © 2009 Blue Mirror")]
+[assembly: AssemblyCopyright("Copyright © 2009 Roman Kowaliszyn")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.2")]
+[assembly: AssemblyFileVersion("2.0.0.2")]
 [assembly: NeutralResourcesLanguageAttribute("")]
