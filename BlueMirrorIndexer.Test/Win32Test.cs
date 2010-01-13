845a5067ae06791c7685e284c1f9006bd8f87a63
SND\kowaliszyn_cp <SND\kowaliszyn_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2010-01-13 10:28:42 +0000

    Tests
    git-svn-id: https://octopi.svn.codeplex.com/svn@36259 a83551a4-30f6-4d81-a974-c6ced450ddbf

---
+++ BlueMirrorIndexer.Test/Win32Test.cs
@@ -0,0 +1,76 @@
+﻿using BlueMirror.Commons;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace BlueMirrorIndexer.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for Win32Test and is intended
+    ///to contain all Win32Test Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class Win32Test
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext {
+            get {
+                return testContextInstance;
+            }
+            set {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for GetFolderIcon
+        ///</summary>
+        [TestMethod()]
+        public void GetFolderIconTest() {
+            string fullPath = "c:\\";
+            Win32.FileIconSize size = Win32.FileIconSize.Large;
+            Icon actual;
+            actual = Win32.GetFolderIcon(fullPath, size);
+            Assert.AreNotEqual(null, actual);
+        }
+    }
+}
