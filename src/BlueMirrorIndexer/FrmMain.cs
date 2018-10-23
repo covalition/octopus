d0c8132e86800ded6a0820623d261e3afdb278cb
Roman <kowaliszyn@gazeta.pl>
2018-10-23 23:37:51 +0200

    Correct the ClickOnce update process

--- src/BlueMirrorIndexer/FrmMain.cs
+++ src/BlueMirrorIndexer/FrmMain.cs
@@ -214,63 +214,66 @@ namespace BlueMirrorIndexer
         }
 
         private void cmCheckForUpdates_Click(object sender, EventArgs e) {
-            UpdateCheckInfo info = null;
+            try {
+                if (ApplicationDeployment.IsNetworkDeployed) {
+                    ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
 
-            if (ApplicationDeployment.IsNetworkDeployed) {
-                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
-
-                try {
-                    info = ad.CheckForDetailedUpdate();
-
-                }
-                catch (DeploymentDownloadException dde) {
-                    MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message, ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
-                    return;
-                }
-                catch (InvalidDeploymentException ide) {
-                    MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message, ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
-                    return;
-                }
-                catch (InvalidOperationException ioe) {
-                    MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message, ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
-                    return;
-                }
-
-                if (info.UpdateAvailable) {
-                    bool doUpdate = true;
-
-                    if (!info.IsUpdateRequired) {
-                        DialogResult dr = MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-                        if (!(DialogResult.OK == dr)) {
-                            doUpdate = false;
-                        }
+                    UpdateCheckInfo info = null;
+                    try {
+                        info = ad.CheckForDetailedUpdate();
                     }
-                    else {
-                        // Display a message that the app MUST reboot. Display the minimum required version.
-                        MessageBox.Show("This application has detected a mandatory update from your current " +
-                            "version to version " + info.MinimumRequiredVersion.ToString() +
-                            ". The application will now install the update and restart.",
-                            "Update Available", MessageBoxButtons.OK,
-                            MessageBoxIcon.Information);
+                    catch (DeploymentDownloadException dde) {
+                        MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message, ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                        return;
+                    }
+                    catch (InvalidDeploymentException ide) {
+                        MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message, ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                        return;
+                    }
+                    catch (InvalidOperationException ioe) {
+                        MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message, ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                        return;
                     }
 
-                    if (doUpdate) {
-                        try {
-                            ad.Update();
-                            MessageBox.Show("The application has been upgraded, and will now restart.", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            Application.Restart();
+                    if (info.UpdateAvailable) {
+                        bool doUpdate = true;
+
+                        if (!info.IsUpdateRequired) {
+                            DialogResult dr = MessageBox.Show($"An update ({info.AvailableVersion.ToString()}) is available. Would you like to update the application now?", "Update Available", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                            if (!(DialogResult.OK == dr)) {
+                                doUpdate = false;
+                            }
                         }
-                        catch (DeploymentDownloadException dde) {
-                            MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde, ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
-                            return;
+                        else {
+                            // Display a message that the app MUST reboot. Display the minimum required version.
+                            MessageBox.Show("This application has detected a mandatory update from your current " +
+                                "version to version " + info.MinimumRequiredVersion.ToString() +
+                                ". The application will now install the update and restart.",
+                                "Update Available", MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                        }
+
+                        if (doUpdate) {
+                            try {
+                                ad.Update();
+                                MessageBox.Show("The application has been upgraded, and will now restart.", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                Application.Restart();
+                            }
+                            catch (DeploymentDownloadException dde) {
+                                MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde, ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                                return;
+                            }
                         }
                     }
+                    else
+                        MessageBox.Show("This application is up to date.", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
-                    MessageBox.Show("This application is up to date.", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("This installation is not network deployed.", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
             }
-            else
-                MessageBox.Show("This installation is not network deployed.", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message, ProductName, MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
         }
 
         #endregion
